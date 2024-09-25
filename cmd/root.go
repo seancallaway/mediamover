39e76e6be068dd89c6066105b902a76d5e0b12f7
Sean Callaway <seancallaway@gmail.com>
2024-09-24 17:57:48 -0700

    Switch to cobra/viper.

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var configFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "mediamover",
+	Short: "mediamover is a tool for organizing your media files",
+	Long: `A tool for identifying and renaming movie and television show
+    files using The Movie Database (https://themoviedb.org)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// do stuff here.
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(loadConfig)
+
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "config.ini", "The configuration file")
+}
+
+func loadConfig() {
+	viper.SetConfigFile(configFile)
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Unable to load config file:", viper.ConfigFileUsed())
+	}
+}
--- mediamover.go
+++ mediamover.go
@@ -1,38 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/urfave/cli"
+	"github.com/seancallaway/mediamover/cmd"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "mediamover"
-	app.Usage = "Rename and move movie and TV files"
+	cmd.Execute()
+	/*
+		app := cli.NewApp()
+		app.Name = "mediamover"
+		app.Usage = "Rename and move movie and TV files"
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "config, c",
-			Value: "config.json",
-			Usage: "Load configuration from `FILE`",
-		},
-	}
+		app.Flags = []cli.Flag{
+			cli.StringFlag{
+				Name:  "config, c",
+				Value: "config.json",
+				Usage: "Load configuration from `FILE`",
+			},
+		}
 
-	app.Action = func(c *cli.Context) error {
-		config, err := NewConfig(c.String("config"))
+		app.Action = func(c *cli.Context) error {
+			// Load configuration file
+			config, err := NewConfig(c.String("config"))
+			if err != nil {
+				return cli.NewExitError(err, 1)
+			}
+
+			// TODO: Get list of tiles to process.
+			if c.Args().Len() == 0 {
+				return cli.NewExitError("Missing required arguments.")
+			}
+			loadPath := c.Args().Get(0)
+
+			// TODO: Copy/move file.
+
+			fmt.Println("This doesn't do anything yet. It would load media from " + loadPath + ".")
+			fmt.Println(config.TvRoot)
+			return nil
+		}
+
+		err := app.Run(os.Args)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("This doesn't do anything yet. Sorry.")
-		fmt.Println(config.TvRoot)
-		return nil
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	*/
 }
